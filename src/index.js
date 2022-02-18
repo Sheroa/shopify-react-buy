3cfa28ad4eee2a2d787f6d675bcb38fbb70cf96a
Sheroa <522libei@163.com>
2022-02-18 15:11:44 +0800

    add google analytics2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-Z8CYZLE3Y4')
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
