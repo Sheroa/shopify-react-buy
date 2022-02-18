baaef79f8f5eb9f034f21123b140e6ff5d4e9584
Sheroa <522libei@163.com>
2022-02-18 15:30:02 +0800

    add google analytics2

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,6 @@ import React, { useContext, useEffect } from 'react'
 import { ShopContext } from '../context/shopContext'
 import { Container, Text, Div, Row, Col } from 'atomize'
 import { Link } from 'react-router-dom'
-import ReactGA from 'react-ga'
-
-// Global site tag (gtag.js) - Google Analytics
-ReactGA.initialize('G-L87M8HXQ58')
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 const HomePage = () => {
 
