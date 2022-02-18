0f80828b323b465fcf9378ab897bc53af6952b9e
Sheroa <522libei@163.com>
2022-02-18 14:21:18 +0800

    modify the relative path

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ import { BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom'
 import ShopProvider from '../context/shopContext';
 import HomePage from '../pages/HomePage'
 import ProductPage from '../pages/ProductPage';
-import Navbar from './Navbar';
-import Cart from './Cart';
+import Navbar from '../components/Navbar';
+import Cart from '../components/Cart';
 
 const debug =
   process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
